54e9bdf2fdd825f8b956841dc5b866d9aaf11493
Kristen Won <kjwon@usc.edu>
2024-02-27 06:34:51 +0000

    did some stuff on codio

--- llrec.cpp
+++ llrec.cpp
@@ -3,5 +3,21 @@
 //*********************************************
 // Provide your implementation of llpivot below
 //*********************************************
-
-
+void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot)
+{
+  if(head == NULL)
+  {
+    smaller = NULL;
+    larger = NULL;
+    return;
+  }
+  if(head->val <= pivot)
+  {
+    smaller = head;
+    llpivot(head->next, smaller->next, larger, pivot);
+  }
+  else {
+    larger = head;
+    llpivot(head->next, smaller, larger->next, pivot);
+  }
+}
